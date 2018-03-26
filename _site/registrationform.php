d2f5d4a1d8ee060a6fd8c14c90da2d71994c6342
administrator@international-aset.com <ICNFAicepr1120>
2018-03-26 13:14:51 -0400

    Fixed registration form issue

--- _site/registrationform.php
+++ _site/registrationform.php
@@ -45,8 +45,6 @@ Thank you for registering for FFHMT 2018. If you have requested any official let
 
 If you are an author, please make sure to send us your camera ready version and a signed copyright form via email to info@ffhmt.com. You can find the copyright form here: https://ffhmt.com/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
 
-Thank you for registering for ICGRE 2018. If you have requested any official letters, please allow up to 5 business days to receive your documents.
-
 To reserve a room at a discounted price, please make your reservation by using this link: http://aset.americananiagara.com
 
 You can find your registration details below. If there are any errors in the information you have provided, please write an email to us at registration@ffhmt.com mentioning the correct information. Please note that you SHOULD NOT refill the form.
