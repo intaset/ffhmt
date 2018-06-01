4511dcda9546d38af50c9b85a8e8b0372d551552
administrator@international-aset.com <>
2018-06-01 12:20:33 -0400

    Added venue

--- _site/registrationform.php
+++ _site/registrationform.php
@@ -186,7 +186,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 </div>
         <div class="bg">
           <h1>6<sup>th</sup>  International Conference on Fluid Flow, <br>Heat and Mass Transfer (FFHMT'19)</h1>
-          <p class="subhead">June 5 - 7, 2019 | Ottawa, Canada</p>
+          <p class="subhead">June 17 - 19, 2019 | Ottawa, Canada</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
@@ -213,7 +213,7 @@ mail($your_email,$your_subject,$message,$your_headers);
 
         <div class="bg">
           <h1>6<sup>th</sup> International Conference on Fluid Flow,<br>Heat and Mass Transfer (FFHMT'19)</h1>
-          <p class="subhead">June 5 - 7, 2019 | Ottawa, Canada</p>
+          <p class="subhead">June 17 - 19, 2019 | Ottawa, Canada</p>
 
           <a href="../papers" class="bg-link">Submissions</a> <p class="dot">&middot;</p> <a href="../dates" class="bg-link">Important Dates</a> <p class="dot">&middot;</p> <a href="../registration" class="bg-link">Registration</a>
 
